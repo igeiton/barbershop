49ded09d767ef3c9e309e5367d0b0fd5e2c3ef92
unknown <freskounity@gmail.com>
2024-06-16 18:39:47 +0700

    fix day_loading

--- src/App/Service/Day.tsx
+++ src/App/Service/Day.tsx
@@ -1,41 +1,79 @@
 import { Button } from '@mui/material';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { useCreateDayMutation, useGetDaysQuery } from '../../Store/API/daysApi';
-import { hasDay } from './Actions/HasDay';
-import Records from './Records';
-import './ServiceStyles/Day.css';
-import Loading from './UI/Loading';
+import {
+    useCreateDayMutation,
+    useGetDayQuery,
+    useLazyGetDaysQuery,
+} from '../../Store/API/daysApi';
 import { useAppSelector } from '../../Store/store';
+import Loading from '../UI/Loading';
+import { hasDay } from './Actions/HasDay';
+import Records from './Client/Records';
 import OwnerRecords from './Owner/OwnerRecords';
+import './ServiceStyles/Day.css';
 
 export default function Day() {
+    //
+    // hooks
+    const navigate = useNavigate();
+    const path = useLocation().pathname.replace('/', '');
+
+    const [asdday, setasdDay] = useState({
+        id: path,
+        day: path,
+        records: [],
+    });
+
     const { isOwner } = useAppSelector((state) => state.user);
 
-    const path = useLocation().pathname.replace('/', '');
-    const { data: days = [], isLoading } = useGetDaysQuery('');
+    // const [trigger, { data: days = [], isLoading, isSuccess }] =
+    //     useLazyGetDaysQuery();
+    const { data: day = [], isSuccess, isError } = useGetDayQuery(path);
     const [createDay] = useCreateDayMutation();
-    const navigate = useNavigate();
 
-    const day = hasDay(path, days);
+    // const
+    // const day = hasDay(path, days);
 
+    // validation
     if (path.replace(/-/g, '').length !== 8 || path.length !== 10) {
         return <div className="w-full self-center text-center">Error</div>;
-        // добавить page with error
+        // добавить page with error !!!!!!!!!!!!!!!!!!!!!
     }
+    // useEffect(() => {
+    //     // get actually days
+    //     trigger('');
+
+    //     // create day if records is empty
+    //     if (day.length === 0) {
+    //     }
+    // }, [path]);
+
+    // useEffect(() => {
+    //     console.log(isSuccess);
+    //     if (isSuccess && day.length === 0) {
+    //         createDay({
+    //             id: path,
+    //             day: path,
+    //             records: [],
+    //         });
+    //         console.log('Create new Day');
+    //         console.log(day);
+    //     }
+    // }, [isSuccess]);
+
+    // if (isLoading) return <div>loading..........</div>;
 
     useEffect(() => {
-        if (day.length === 0) {
-            createDay({
-                day: path,
-                records: [],
-            });
-            console.log('Create new Day');
+        if (isSuccess) {
+            console.log('success');
+            setasdDay(day);
         }
-    }, [path]);
 
-    if (isLoading)
-        return <div className="w-full self-center text-center">Loading...</div>;
+        if (isError) {
+            console.log('error');
+        }
+    }, [isSuccess, isError]);
 
     return (
         <div className="flex flex-col w-full max-w-[100vw] p-[15px] gap-5">
@@ -47,7 +85,9 @@ export default function Day() {
                 {'<< Go Back'}
             </Button>
 
-            {!isOwner && (
+            <Records day={asdday} days={asdday.id} />
+
+            {/* {!isOwner && (
                 <>
                     {day.length !== 0 && <Records day={day} days={days} />}
 
@@ -61,7 +101,7 @@ export default function Day() {
 
                     {day.length === 0 && <Loading />}
                 </>
-            )}
+            )} */}
         </div>
     );
 }
