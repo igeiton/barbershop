d81ab33f052775a85969c979b72907c3641672fd
unknown <freskounity@gmail.com>
2024-06-22 00:41:08 +0700

    rename sw file

--- dist/registerSW.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/OneSignalSDKWorker.js.js', {
-            scope: '/',
-        });
-    });
-}
