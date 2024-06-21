91b1a02b1f042fc9713bc26fa39884c4e9cf5a88
unknown <freskounity@gmail.com>
2024-06-22 02:12:15 +0700

    inline script again

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     plugins: [
         react(),
         VitePWA({
-            injectRegister: 'inline',
+            injectRegister: 'script-defer',
         }),
     ],
 });
