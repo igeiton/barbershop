56cd64e68dd0d3f04317e29b32eebffcf630f7fe
unknown <freskounity@gmail.com>
2024-06-21 22:15:08 +0700

    change app_id and delete console logs

--- src/Store/API/createNotif.ts
+++ src/Store/API/createNotif.ts
@@ -1,17 +1,16 @@
 import axios from 'axios';
 
 export const createNotif = async (subsID: string, text: any, date: Date) => {
-    console.log(subsID, text, date);
     const headers = {
         headers: {
             'Content-Type': 'application/json',
             Authorization:
-                'Basic OWZmMDI3ODQtODIwYS00Nzc1LTg0NmQtMDgzN2JkNWViZjg3',
+                'Basic ZTU2MzY3MDQtYjFkYy00NGYzLTk3ZmQtMTM2OTk5MmVjNDY3',
         },
     };
 
     const body = JSON.stringify({
-        app_id: '7ec78ef9-197a-47a6-b77c-fa6d02907eba',
+        app_id: '8fc8a9b2-0afd-47c6-9799-9cf88b9bc132',
         name: 'Barbershop',
         headings: { en: text.title },
         contents: { en: text.subtitle },
@@ -19,12 +18,11 @@ export const createNotif = async (subsID: string, text: any, date: Date) => {
         send_after: date,
     });
 
-    axios
-        .post(
-            'https://onesignal.com/api/v1/notifications?app_id=7ec78ef9-197a-47a6-b77c-fa6d02907eba',
-            body,
-            headers
-        )
-        .then((data) => console.log(data))
-        .catch((err) => console.log(err));
+    axios.post(
+        'https://onesignal.com/api/v1/notifications?app_id=8fc8a9b2-0afd-47c6-9799-9cf88b9bc132',
+        body,
+        headers
+    );
+    // .then((data) => console.log(data))
+    // .catch((err) => console.log(err));
 };
