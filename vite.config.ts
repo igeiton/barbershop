34e0915814f6bb19cf474f4e0b71bc07ced11847
unknown <freskounity@gmail.com>
2024-06-22 00:19:10 +0700

    change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            input: {
+                index: 'index.html',
+                main: './src/OneSignalSDKWorker.js',
+            },
+            output: {
+                entryFileNames: 'OneSignalSDKWorker.js',
+            },
+        },
+    },
+});
